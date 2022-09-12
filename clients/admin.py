d6fc4482b2044e38dc2dd22352acb5745b6fd9da
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-12 20:20:33 -0300

    add admin to client

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'address', 'country', 'city']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'country']
+    list_per_page = 20
+
+admin.site.register(Client, ClientAdmin)
+
+
