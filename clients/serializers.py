505a6e8ff78ddf6b44c5e46797ebb3f9e1cf2e6c
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-12 20:27:57 -0300

    create client serializer

---
+++ clients/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Client
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ['name', 'address', 'city', 'country']
