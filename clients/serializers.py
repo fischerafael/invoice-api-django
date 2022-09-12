6daac43c3031e730847141a5f016e0dd1758e381
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-12 20:30:25 -0300

    add viewset

--- clients/serializers.py
+++ clients/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Client
 
-class UserSerializer(serializers.ModelSerializer):
+class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ['name', 'address', 'city', 'country']
