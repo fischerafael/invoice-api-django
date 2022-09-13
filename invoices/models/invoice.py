504b5e358bba7a75bcb85cc1976d3d34c1828907
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-12 21:42:20 -0300

    add payment terms as a tuple

--- invoices/models/invoice.py
+++ invoices/models/invoice.py
@@ -3,10 +3,12 @@ from django.db import models
 from clients.models import Client
 from invoices.models.currency import Currency
 
+PAYMENT_TERMS = [(1, 'EOB'), (2, 'EOM')]
 
 class Invoice(models.Model):
     date = models.DateField(auto_now=True)
     total = models.IntegerField(null=False) 
+    paymentTerms = models.CharField(max_length=20,choices=PAYMENT_TERMS, default='EOB')
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)  
     client = models.ForeignKey(Client, on_delete=models.CASCADE)     
 
