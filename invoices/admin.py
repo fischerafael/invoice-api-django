cac8c1c7b15836dfdbe616dec2eb50159ef7e7ca
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-12 21:08:16 -0300

    working with currencies

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Invoice, Currency
 
-# Register your models here.
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'date', 'currency', 'total', 'client']
+    list_display_links = ['id', 'client']
+    search_fields = ['id', 'total']
+    list_per_page = 20
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['id', 'name']
+    list_per_page = 20
+
+admin.site.register(Invoice, InvoiceAdmin)
+admin.site.register(Currency, CurrencyAdmin)
--- invoices/models.py
+++ invoices/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 from clients.models import Client
 
-class Invoice(models.Model):
-    date = models.DateField(auto_now=True)
-    currency = models.TextField(max_length=120)
-    total = models.IntegerField(null=False) 
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)  
+class Currency(models.Model):   
+    name = models.TextField(max_length=10)   
 
     def __str__(self):
-        return self.id
+        return self.name
+
+class Invoice(models.Model):
+    date = models.DateField(auto_now=True)
+    total = models.IntegerField(null=False) 
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)  
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)  
+
+    # def __str__(self):
+    #     return self.date
+
