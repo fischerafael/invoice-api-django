057960afb5bebb83387631f7762011544f1afd87
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-14 20:36:54 -0300

    feat: add Service model

---
+++ invoices/models/service.py
@@ -0,0 +1,9 @@
+from sqlite3 import Date
+from django.db import models
+
+class Service(models.Model):
+    title = models.CharField(max_length=20)
+    dayRate = models.FloatField(default=0)
+
+    def __str__(self) -> str:
+        return str(self.title)
